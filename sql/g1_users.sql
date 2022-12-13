4b88505cd236a5cf47a797122dffcb49440b2fd2
Anton Gladyshev <returnstrike@yandex.com>
2022-12-13 17:58:26 +0200

    added some stuff

--- sql/g1_users.sql
+++ sql/g1_users.sql
@@ -1,4 +1,4 @@
 USE users_global;
 CREATE USER IF NOT EXISTS 'clustered_user'@'%' IDENTIFIED BY 'password';
-GRANT USAGE ON *.* TO 'clusterd_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0;
-GRANT ALL PRIVILEGES ON `sailmaster`.* TO 'clusterd_user'@'%';
+GRANT USAGE ON *.* TO 'clustered_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0;
+GRANT ALL PRIVILEGES ON `users_global`.* TO 'clustered_user'@'%';
--- sql/us1_users.sql
+++ sql/us1_users.sql
@@ -1,5 +1,5 @@
 USE users_us;
 CREATE USER IF NOT EXISTS 'clustered_user'@'%' IDENTIFIED BY 'password';
-GRANT USAGE ON *.* TO 'clusterd_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0;
-GRANT ALL PRIVILEGES ON `sailmaster`.* TO 'clusterd_user'@'%';
+GRANT USAGE ON *.* TO 'clustered_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0;
+GRANT ALL PRIVILEGES ON `users_us`.* TO 'clustered_user'@'%';
 

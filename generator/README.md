8b4dce0cf76835600e7c9b605bbd15b3fed547bc
Anton Gladyshev <returnstrike@yandex.com>
2022-12-13 13:48:42 +0200

    draft of generator

---
+++ generator/README.md
@@ -0,0 +1,1 @@
+Generator is a small program to init the database and to fill it with users
---
+++ generator/gen.py
@@ -0,0 +1,10 @@
+names = ["James", "Mary", "Robert", "Patricia", "John", "Jennifer",
+"Michael", "Linda", "David", "Elizabeth", "William", "Barbara",
+"Richard", "Susan", "Joseph", "Jessica", "Thomas", "Sarah",
+"Charles", "Karen", "Christopher", "Lisa", "Daniel", "Nancy",
+"Matthew", "Betty", "Anthony", "Margaret", "Mark", "Sandra"]
+surnames = ["Black", "Brown", "Smith", "Miller", "Green", "Johnson", "Williams",
+"Jones", "Davis", "Wilson", "Anderson", "Taylor", "Moore", "Jackson", "Martin",
+"Thompson", "White", "Harris", "Clark", "Lewis", "Robinson", "Walker", "Young",
+"Allen", "King", "Scott", "Hill", "Adams", "Nelson", "Baker", "Hall", "Campbell",
+"Mitchell", "Carter", "Roberts"]

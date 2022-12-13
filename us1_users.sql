f9a28a070cb56acf07515266e5d1ae2ba1b7a508
Anton Gladyshev <returnstrike@yandex.com>
2022-12-13 11:33:39 +0200

    done db creation; user creation;

---
+++ us1_users.sql
@@ -0,0 +1,5 @@
+USE users_us;
+CREATE USER IF NOT EXISTS 'clustered_user'@'%' IDENTIFIED BY 'password';
+GRANT USAGE ON *.* TO 'clusterd_user'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0;
+GRANT ALL PRIVILEGES ON `sailmaster`.* TO 'clusterd_user'@'%';
+
